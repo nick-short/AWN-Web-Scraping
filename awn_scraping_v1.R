4e60ba6bbedfb077e854123b6db51e3f254cae01
nick-short <40464916+nick-short@users.noreply.github.com>
2018-10-13 12:06:44 -0400

    Add files via upload

---
+++ awn_scraping_v1.R
@@ -0,0 +1,135 @@
+#### Access World News Scraping Code ####
+### Written by Matthew Colón and Nicholas Short ###
+
+
+rm(list = ls()) # Clear the workspace
+
+
+          ## Libraries.  Note: we should probably use packrat for version control
+
+
+#Use these commands to install RSelenium if needed.  Note: you have to install a
+#Java development kit to make it run.  See:
+#https://www.oracle.com/technetwork/java/javase/downloads/jdk10-downloads-4416644.html
+
+#library(devtools) install_version("binman",
+#version = "0.1.0", repos = "https://cran.uni-muenster.de/")
+#install_version("wdman", version = "0.2.2", repos =
+#"https://cran.uni-muenster.de/") install_version("RSelenium", version =
+#"1.7.1", repos = "https://cran.uni-muenster.de/")
+
+library(readr)
+library(RSelenium)
+#library(rvest) # Do we actually use this?
+#library(stringi) # Do we actually use this?
+library(stringr) # We use str_split() and and str_replace() in get_number()
+library(profvis) # We use pause()
+
+
+          ## Source Primary Functions
+
+
+source('primary_functions.R')
+
+
+          ## Execute Web Scraping
+
+
+## Create a vector of key search and ensure that entries with multiple words are
+## in the proper format for constructing URLs
+words <- c("driverless car", "autonomous vehicle", "self-driving")
+words <- format_words(words)
+word_string <- make_string(words)
+
+## Define first month and year and last month and year of search queries
+first_month <- "Jan"
+first_year <- 2018
+last_month <- "Aug"
+last_year <- 2018
+
+## Construct search term URLs (urls) for all dates and baseline urls (base_urls)
+## for determining total number of stories (with no search terms)
+urls <- generate_urls(first_month, first_year, last_month, last_year, word_string)
+base_urls <- generate_baseline_urls(first_month, first_year, last_month, last_year, word_string)
+
+## Initialize variables for storing data
+hits_data <- count_data(first_month, first_year, last_month, last_year)
+sample_text <- list()
+
+## Enter Stanford login information
+usr <- "nshort"
+pwd <- "Inv!sibl3Bridge"
+
+## Initiate automated Google Chrome window ("remDr")
+driver<- rsDriver()
+remDr <- driver[["client"]]
+
+## Try to navigates to the first URL
+remDr$navigate(urls[1])
+
+## If this re-directs to login.stanford.edu, enter two-factor authorization info
+## (DUO push still not enabled)
+current_url <- unlist(remDr$getCurrentUrl())
+if (grepl("login.stanford.edu", current_url)){twofa_login(usr,pwd)}
+
+## You have to pause here to resolve duo push if needed.
+
+## Load each URL and scrape the number of hits plus text snippets (get_snippets
+## = sample_text).  Note: we will eventually want a function that runs a for
+## loop on execute_queries for each set of search terms and then combines the
+## results, but for now let's just execute 9 blocks of code.
+start <- Sys.time()
+query_results1 <- execute_queries(urls, hits_data, text_list = sample_text, nsnip = 10)
+fin <- Sys.time()
+fin - start
+
+## Do the same for the baseline URLs, for normalization.  The reason for doing
+## this separately is that it only needs to be executed once, as the raw monthly
+## numbers will be the same (they do not contain search terms and will not vary
+## by search terms).
+start <- Sys.time()
+baseline_results <- execute_queries(base_urls, hits_data)
+fin <- Sys.time()
+fin - start
+
+## Close the automated Chrome window
+remDr$close()
+
+
+          ## Save results.
+
+
+## Save results to the current working directory.  Note: we want to think about
+#how to do this in an automated way.  It might be best to run all the queries
+#and then save.image(), or we might want to run them one at a time and save to a .csv.
+#write.csv(query_results1, file = "QueryResultsTable.csv")
+
+
+          ## Analyze results.
+
+
+library(zoo)
+library(ggplot2)
+
+## Shape the data.  Convert dates into year-months.  Calculate hits relative to
+## total news stories (from baseline_results) or a moving average of total results.
+
+hits$date <- as.yearmon(hits$date, "%d%b%Y")
+hits$prop <- hits$count / baseline_results$count
+baseline_results$count_ma <- NA
+ma_window <- 13 # Length of 
+baseline_results$count_ma[ma_window:(nrow(baseline_results) - ma_window + 1)] <- rollmean(baseline_results$count, ma_window)
+hits$prop_ma <- hits$count / baseline_results$count_ma
+
+# This overlays the plots of the monthly average (blue) and 13-month moving
+# average (red) of the query in question. It is likely that these won't be the
+# plots we end up overlaying, but we have been discussing the matter of which is
+# better to use, so at the moment, these plots are overlayed for comparison.
+p <- ggplot(hits) + xlab("Time") + ylab("Proportion of Results") +
+  labs(title = 'Proportion of Results for "Cloud Computing" on Access World News Time Series') +
+  geom_line(aes(x = date, y = prop_ma, colour = "Yearly_Moving_Average")) + 
+  geom_line(aes(x = date, y = prop, colour = "Standard_Monthly_Proportion")) + 
+  scale_color_manual(name = "Type of Proportion", 
+                     values = c(Yearly_Moving_Average = "red", 
+                                Standard_Monthly_Proportion = "dark blue"))
+
---
+++ primary_functions.R
@@ -0,0 +1,240 @@
+## This function will format the provided search 'words' by substituting "+" for
+## white space
+
+format_words <- function(terms){return(as.character(sapply(X = words, FUN = function(y){y <- gsub(" ", "+", y, fixed = TRUE)})))}
+
+## This function will take a vector of search words and create a single string
+## to be used in constructing the URL
+
+make_string <- function(word_vector){
+  rep_str <- "%22"
+  word_vector <- paste(rep_str, word_vector, rep_str, sep = "")
+  word_string <- word_vector[1]
+  for(i in 2:length(word_vector)) {word_string <- paste(word_string, word_vector[i], sep = "+%7C+")}
+  return(word_string)
+}
+
+## This function will generate a full set of monthly URLS from 'beg_year' to
+## 'end_year' given a formatted string of 'search_words'
+
+generate_urls <- function(beg_month, beg_year, end_month, end_year, search_words){
+  months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+  years <- as.character(c(beg_year:end_year))
+  
+  months_rep <- rep(months, times = length(years))
+  years_rep <- rep(years, each = length(months))
+  all_dates <- paste(months_rep, "+", years_rep, sep = "")
+  
+  url_prefix <- 'https://infoweb-newsbank-com.stanford.idm.oclc.org/resources/search/nb?p=AWNB&b=results&action=search&fld0=YMD_date&val0='
+  url_middle <- '&bln1=AND&fld1=alltext&val1='
+  url_suffix <- '&bln2=OR&fld2=alltext&val2=&bln3=OR&fld3=alltext&val3=&sort=YMD_date%3AD'
+  
+  urls <- paste(url_prefix, all_dates, url_middle, search_words, url_suffix, sep = "")
+  #urls <- gsub("\\\\", "", urls, fixed = TRUE)
+  
+  first_element <- which(all_dates == paste(beg_month, "+", beg_year, sep = ""))
+  last_element <- which(all_dates == paste(end_month, "+", end_year, sep = ""))
+  urls <- urls[first_element:last_element]
+  
+  return(urls)
+}
+
+## This function will generate a full set of monthly URLS from 'beg_year' to
+## 'end_year' with no search words (for pulling total counts)
+
+generate_baseline_urls <- function(beg_month, beg_year, end_month, end_year, search_words){
+  months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+  years <- as.character(c(beg_year:end_year))
+  
+  months_rep <- rep(months, times = length(years))
+  years_rep <- rep(years, each = length(months))
+  all_dates <- paste(months_rep, "+", years_rep, sep = "")
+  
+  url_prefix <- 'https://infoweb-newsbank-com.stanford.idm.oclc.org/resources/search/nb?p=AWNB&b=results&action=search&fld0=YMD_date&val0='
+  url_suffix <- '&bln1=AND&fld1=YMD_date&val1=&sort=YMD_date%3AD'
+  
+  urls <- paste(url_prefix, all_dates, url_suffix, sep = "")
+  #urls <- gsub("\\\\", "", urls, fixed = TRUE)
+  
+  first_element <- which(all_dates == paste(beg_month, "+", beg_year, sep = ""))
+  last_element <- which(all_dates == paste(end_month, "+", end_year, sep = ""))
+  urls <- urls[first_element:last_element]
+  
+  return(urls)
+}
+
+
+## This function will generate a data frame for storing the monthly counts with
+## the 'date' variable in .Date format
+
+count_data <- function(beg_month, beg_year, end_month, end_year){
+  
+  months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+  years <- as.character(c(beg_year:end_year))
+  
+  months_rep <- rep(months, times = length(years))
+  years_rep <- rep(years, each = length(months))
+  all_dates <- paste("1", months_rep, years_rep, sep = "")
+  all_dates <- as.Date(all_dates, format = "%d%b%Y")
+  
+  first_element <- which(all_dates == as.Date(paste("1", beg_month, beg_year, sep = ""), format = "%d%b%Y"))
+  last_element <- which(all_dates == as.Date(paste("1", end_month, end_year, sep = ""), format = "%d%b%Y"))
+  all_dates <- all_dates[first_element:last_element]
+  
+  count_df <- data.frame(date = all_dates, count = rep(NA, length(all_dates)))
+}
+
+## This function well enter your username and password and advance the browser
+## through the first stage of two-factor authentication.  Right now you have to
+## manually pass the second stage.
+
+twofa_login <- function(username, password){
+  
+  username_box <- remDr$findElement(using = 'css selector', "#username")
+  username_box$sendKeysToElement(list(username))
+  
+  password_box <- remDr$findElement(using = 'css selector', "#password")
+  password_box$sendKeysToElement(list(password))
+  
+  login_button <- remDr$findElement(using = 'css selector', ".submit-button")
+  login_button$clickElement()
+  
+  # Need a line for the DUO push too (if that page pops up); for now enter manually
+}
+
+## This function will retrieve the number of search results
+
+get_number <- function(){
+  
+  # Determine the number of search results and assign it to the variable "num"
+  text_line <- remDr$findElement(using = 'css selector', ".nb-showing-result-count")
+  text_num <- as.character(text_line$getElementText())
+  
+  # Text takes the form of "Showing 1-X of Y Results" where Y, the desired
+  # number, is the fourth word. Extract Y, then remove commas and convert to
+  # numeric format
+  num <- unlist(str_split(text_num, " "))[4]
+  num <- as.numeric(str_replace(num, ",", ""))
+  return(num)
+}
+
+
+# This function assumes we are on the first page of search query.  It will
+# re-sort the data so that the best matches, rather than the most recent
+# matches, are displayed first.  It will then change the page options so that
+# 50, rather than 10, results are displayed on each page.  It will then retrieve
+# 'num' text snippets.  If num is greater than 50, the function will
+# sequentially advance the browser page by page and extract the text snippets
+# shown on each page.
+
+get_snippets <- function(num){
+  snippets <- rep(NA, num)
+  count <- 1
+  rpp <- 10 # Define number of results per page
+  
+  # Re-sort so that the best matches are displayed first
+  
+  
+  # Change page options to display 50 result per page
+  
+  
+  # If there are rpp or less results then simply take text snippets from the
+  # num results; otherwise iterate through each page
+  
+  if (num <= rpp) {
+    
+    for(j in 1:num) {
+      css <- paste("li:nth-child(", j, ") .preview", sep = "")
+      text_box <- remDr$findElement(using = 'css selector', css)
+      text_snippet <- as.character(text_box$getElementText())
+      snippets[count] <- text_snippet
+      count <- count + 1
+    }
+    
+  } else {
+    
+    #start <- Sys.time()
+    num_floor <- floor(num / rpp)
+    for(k in 1:(num_floor)) {
+      
+      for(j in 1:rpp) {
+        css <- paste("li:nth-child(", j, ") .preview", sep = "")
+        text_box <- remDr$findElement(using = 'css selector', css)
+        text_snippet <- as.character(text_box$getElementText())
+        snippets[count] <- text_snippet
+        count <- count + 1
+      }
+      
+      next_button <- remDr$findElement(using = 'css selector', ".pager-next a")
+      next_button$clickElement()
+      
+    }
+    
+    # The last page will contain (num %% rpp) results
+    
+    for(l in 1:(num %% rpp)) {
+      css <- paste("li:nth-child(", l, ") .preview", sep = "")
+      text_box <- remDr$findElement(using = 'css selector', css)
+      text_snippet <- as.character(text_box$getElementText())
+      snippets[count] <- text_snippet
+      count <- count + 1
+    }
+    
+    #fin <- Sys.time()
+    #fin - start  
+    
+  }
+  
+  return(snippets)
+  
+}
+
+
+## This function will query each url in 'url_vector', obtain the total number of
+## news stories generated from that query using get_number(), and save the
+## results to the corresponding row in the data frame 'hits'.  If an empty list
+## is passed to the function, it will also extrapolate 'nsnip' text snippets
+## using get_snippets().  If 'nsnip' is not specified, it will extrapolate all
+## snippets.  It takes a pause of random length at the end of each query. The
+## output will depend on how the function is called: (1) if only scraping hits,
+## the 'hits' data frame will be returned; (2) if also scraping snippets, a list
+## with the 'hits' data frame as the first element and the list of snippets as
+## the second element will be returned.
+
+execute_queries <- function(url_vector, hits, text_list = NULL, nsnip = NULL){
+  
+  for(i in 1:length(url_vector)) {
+    
+    # Navigate to the URL
+    remDr$navigate(url_vector[i])
+    
+    # We need a try-catch here in case there are no search results. 
+    tryCatch({
+      
+      # Scrape number of hits and, if 'text' is passed to function, scrape snippets too
+      hits$count[i] <- get_number()
+      if (!is.null(text_list)){
+        
+        # If nsnip is not specified, set to total number of hits
+        if (is.null(nsnip)){nsnip <- hits$count[i]} 
+        text_list[[i]] <- get_snippets(nsnip)
+        }
+      
+    }, error = function(e) {
+      
+      # Otherwise set to 0 and NA respectively
+      hits$count[i] <- 0
+      if(!is.null(text_list)){text_list[[i]] <- "NA"}
+      
+    })
+    
+    # Pause before submitting another query.  At the moment, the pause is normally
+    # distributed around 30 seconds, with a SD of 5 seconds.
+    
+    pause(abs(rnorm(1,30,5)))
+    
+  }
+  
+  if(is.null(text_list)){result <- hits} else{result <- list(hits,text)}
+  return(result)
+}
